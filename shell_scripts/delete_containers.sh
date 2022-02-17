cfa9f3d10e3b21d17baf32a08391c8ab3889516a
Binh Tran <tranthaibinh111@yahoo.com>
2022-02-17 14:11:19 +0700

    finish the docker

--- shell_scripts/delete_containers.sh
+++ shell_scripts/delete_containers.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 dir="$(pwd)/shell_scripts"
 
+bash "$dir/delete_reactjs.sh"
 bash "$dir/delete_django.sh"
 bash "$dir/delete_postgres.sh"
--- shell_scripts/delete_django.sh
+++ shell_scripts/delete_django.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 docker container rm -f ai-4-smart-healthcare_django;
+docker image rm -f ai-4-smart-healthcare_django;
 docker volume rm -f ai-4-smart-healthcare_django_ipython_data;
