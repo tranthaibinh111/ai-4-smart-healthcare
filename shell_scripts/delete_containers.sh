8cff92b8f6ef81809fc8983b9fb36bdeb1856356
Binh Tran <tranthaibinh111@yahoo.com>
2022-02-19 10:09:29 +0700

    finish to deployed the docker

--- shell_scripts/delete_containers.sh
+++ shell_scripts/delete_containers.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 dir="$(pwd)/shell_scripts"
 
+bash "$dir/delete_nginx.sh"
 bash "$dir/delete_reactjs.sh"
 bash "$dir/delete_django.sh"
 bash "$dir/delete_postgres.sh"
+docker volume rm -f ai-4-smart-healthcare_static_volume;
