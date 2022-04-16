dc74102e0e659d5e38d54f293dce76c441ccf2bd
Binh Tran <tranthaibinh111@yahoo.com>
2022-04-16 11:57:12 +0700

    edit the scripts

--- shell_scripts/clone_back-end.sh
+++ shell_scripts/clone_back-end.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 mkdir back-end
-git clone git@github.com:tranthaibinh111/ai-4-smart-healthcare_back-end.git ./back-end
+git clone https://github.com/tranthaibinh111/ai-4-smart-healthcare_back-end.git ./back-end
